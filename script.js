d541486d6ec3d6fb748f1e3893b2bcb3fa66caf4
Nandang <77793855+BegalNenen@users.noreply.github.com>
2021-03-05 17:02:57 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery('#responsive-data-table').DataTable({
-      "aLengthMenu": [[10, 20, 30, -1], [10, 20, 30, "All"]],
-      "pageLength": 10,
-      "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
-    });
-  });
