fd794bc43263be7e0751f184b49407dc4a592cd5
Nandang <77793855+BegalNenen@users.noreply.github.com>
2021-03-05 17:05:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,28 +15,28 @@
 <div class="w3-container" style="padding:20px">
 <strong><h4 align="center">DAFTAR TOOLS YANG TERSEDIA</strong></h4>
 <hr>
-<h5><strong><a href="/youtube-to-mp3">Youtube to Mp3</a></strong></h5> 
+<h5><strong><a href="https://lepita.herokuapp.com/youtube-to-mp3">Youtube to Mp3</a></strong></h5> 
 Anda dapat mengonversi video YouTube Anda menjadi format mp3. Hal ini memungkinkan Anda mendengarkan buku audio favorit yang diunduh dari YouTube di Ponsel, Tablet atau Komputer.
 <hr>
-<h5><strong><a href="/facebook-downloader">Facebook Video Downloader</a></strong></h5>
+<h5><strong><a href="https://lepita.herokuapp.com/facebook-downloader">Facebook Video Downloader</a></strong></h5>
 Ini adalah sebuah tools gratis yang bisa membantumu untuk menyimpan berbagai video yang kamu suka di Facebook.
 <hr>
-<h5><strong><a href="/tiktok-downloader">TikTok Downloader</a></strong></h5>
+<h5><strong><a href="https://lepita.herokuapp.com/tiktok-downloader">TikTok Downloader</a></strong></h5>
 TikTok Downloader memungkinkan Anda menyimpan video TikTok ke komputer, tablet, atau ponsel Anda tanpa repot registrasi atau instal aplikasi.
 <hr>
-<h5><strong><a href="/corona">Pantau Corona</a></strong></h5>
+<h5><strong><a href="https://lepita.herokuapp.com/corona">Pantau Corona</a></strong></h5>
 Menampilkan jumlah Pasien yang <i>Terjangkit, Sembuh</i> dan <i>Meninggal</i> secara Realtime berdasarkan Kabupaten/Kota.
 <hr>
-<h5><strong><a href="/text-generator">Text Generator</a></strong></h5>
+<h5><strong><a href="https://lepita.herokuapp.com/text-generator">Text Generator</a></strong></h5>
 Sebuah tools sederhana yang dapat merubah sebuah text menjadi font font unik dan 𝔎𝔢𝔯𝔢𝔫 𝕊𝕖𝕡𝕖𝕣𝕥𝕚 ɨռɨ
 <hr>
-<h5><strong><a href="/password-generator">Password Generator</a></strong></h5>
+<h5><strong><a href="https://lepita.herokuapp.com/password-generator">Password Generator</a></strong></h5>
 adalah aplikasi online untuk membuat password secara acak dengan kombinasi huruf, angka, simbol, huruf besar dan kecil, sehingga sulit untuk dipecahkan oleh hacker.
 <hr>
-<h5><strong><a href="/random-number">Random Number Generator</a></strong></h5>
+<h5><strong><a href="https://lepita.herokuapp.com/random-number">Random Number Generator</a></strong></h5>
 Merupakan tools yang bisa menampilkan angka secara acak dengan menentukan batas minimal dan maksimalnya terlebih dahulu.
 <hr>
-<h5><strong><a href="/al-quran">Al Quran Online</a></strong></h5>
+<h5><strong><a href="https://lepita.herokuapp.com/al-quran">Al Quran Online</a></strong></h5>
 Baca Al Quran Online lengkap dengan latin & terjemah Indonesia. Situs mudah dibaca, cepat dibuka & hemat kuota.
 </div>
 </div>
